422639690740c30a6343fb602976dc0986109828
Adarsh753 <97660683+Adarsh753@users.noreply.github.com>
2024-11-20 15:37:36 +0530

    Input Output practice completed

--- Input_Output_with_Opencv.py
+++ Input_Output_with_Opencv.py
@@ -5,6 +5,13 @@ import cv2
 img_path = r"D:/OpenCv/Resources/Bird.jpg"
 img = cv2.imread(img_path)
 
+#read a video
+Vid_path = r"D:/OpenCv/Resources/big_buck_bunny_480p_1mb.mp4"
+Video = cv2.VideoCapture(Vid_path)
+
+#read from webcam
+webcam = cv2.VideoCapture(0)
+
 #write an image
 img2=cv2.imwrite(r'D:/OpenCv/Resources/Bird_write.jpeg',img)
 
@@ -13,3 +20,27 @@ img2=cv2.imwrite(r'D:/OpenCv/Resources/Bird_write.jpeg',img)
 cv2.imshow('Bird',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#visualize video
+ret =  True
+while ret:
+    ret , frame = Video.read()
+
+    if ret:
+        cv2.imshow('frame',frame)
+        cv2.waitKey(40)
+        
+Video.release()
+cv2.destroyWindow(frame)
+
+#visualize webcam video
+
+
+while True:
+    ret , frame = webcam.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyWindow(frame)
