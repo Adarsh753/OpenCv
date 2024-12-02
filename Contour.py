5e0cbff4dfb81ba08fad259faa20210016e4b5ec
Adarsh753 <97660683+Adarsh753@users.noreply.github.com>
2024-12-02 19:13:20 +0530

    Update Contour.py

--- Contour.py
+++ Contour.py
@@ -2,7 +2,7 @@ import cv2
 img_path = r"D:\OpenCv\Resources\BB.jpg"
 img=cv2.imread(img_path)
 
-img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 ret, thresh = cv2.threshold(img_grey, 127 , 255, cv2.THRESH_BINARY_INV)
 
@@ -11,4 +11,4 @@ contours , hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SI
 cv2.imshow('Img',img)
 cv2.imshow('Img_gry',img_grey)
 cv2.imshow('Img_cont',thresh)
-cv2.waitKey()
+cv2.waitKey(0)
